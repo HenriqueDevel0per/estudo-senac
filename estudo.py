9da0277d90a3121062279d7946f6766dd7d1f047
JORGE HENRIQUE INACIO DE OLIVEIRA <jorge.inacio@aluno.senac.rio>
2025-08-08 19:01:08 -0300

    aula

---
+++ estudo.py
@@ -0,0 +1,16 @@
+
+nm1 = float(input("Digite o primeiro numero: "))
+nm2 = float(input("Digite o segundo numero: "))
+
+
+#processamento das variavéis
+sma = nm1 + nm2
+dbr = nm1 * 2
+mtd = nm1 / 2
+dmn = nm2 - nm1
+
+# Saida resultado
+print("a soma dos números é ",sma)
+print("O dobro do primeiro é ",dbr)
+print("A metade do segundo numero é ",mtd)
+print("A Subtracao é ",dmn)
