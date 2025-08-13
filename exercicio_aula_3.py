9e94f02334b9eb86a47d9f60fb680e0588dbf197
JORGE HENRIQUE INACIO DE OLIVEIRA <jorge.inacio@aluno.senac.rio>
2025-08-13 19:33:00 -0300

    updatecode

--- exercicio_aula_3.py
+++ exercicio_aula_3.py
@@ -1,26 +1,24 @@
 #exercicio_aula_3
 
-print("Pesquisador de valor carros On-line!")
+print("Pesquisador de carros On-line!")
 
-car1 = input("Insiro o modelo do primeiro carro a ser verificado: ")
-vlr1 = float (50000)
+car1 = input("\nInsiro o modelo do primeiro carro a ser verificado: ")
+ida1 = int(input(f"\nDigite o Ano do {car1}: "))
 
-car2 = input("Insiro o modelo do segundo carro a ser verificado: ")
-vlr2 = float(100000)
+car2 = input("\nInsiro o modelo do segundo carro a ser verificado: ")
+ida2 = int(input(f"\nDigite o Ano de {car2}: "))
 
-car3 = input("Insiro o modelo do terceiro carro a ser verificado: ")
-vlr3 = float (90000)
+car3 = input("\nInsiro o modelo do terceiro carro a ser verificado: ")
+ida3 = int(input(f"\nDigite o Ano de {car3}: "))
 
 
 #comparativo
-if vlr1 >= vlr2 and vlr1 >= vlr3:
-    carcaro = car1
-    cust = vlr1
-elif vlr2 >= vlr3:
-    carcaro = car2
-    cust = vlr2
+if ida1 < ida2 and ida1 < ida3:
+    ida1 = 2025 - ida1
+    print(f"\n\nO seu carro mais antigo é {car1} com {ida1} anos")
+elif ida2 < ida3:
+    ida2 = 2025 - ida2
+    print(f"\n\nO seu carro mais antigo é {car2} com {ida2} anos")
 else:
-    carcaro = car3
-    cust = vlr3
-
-print(f"\nO seu carro mais caro é {carcaro} custando R$ {cust}")
+    ida3 = 2025 - ida3
+print(f"\n\nO seu carro mais antigo é {car3} com {ida3} anos")
