2a7d9d6a5e975d7862686b9ade5bbd7766b6fa25
JORGE HENRIQUE INACIO DE OLIVEIRA <jorgehenrique18038@gmail.com>
2025-08-15 18:42:48 -0300

    melhoria no codigo aula 4

--- #aula-4.py
+++ #aula-4.py
@@ -5,34 +5,52 @@ sex1 = str(input(f"\nInsira o sexo de {clb1}, H para Homem e M para Mulher : "))
 estcv1 = str(input(f"\ninsira estado civil de {clb1}: C para Casado e S para Solteiro : "))
 slr1 = float(input(f"\nInsira o salario atual de {clb1} R$ :"))
 
+print("\n")
 
 clb2 = str(input("\ninsira o nome do colaborador 2: "))
 sex2 = str(input(f"\nInsira o sexo de {clb2}, H para Homem e M para Mulher : "))
 estcv2 = str(input(f"\ninsira estado civil de {clb2} C para Casado e S para Solteiro : "))
 slr2 = float(input(f"\nInsira o salario atual de {clb2}: "))
 
+print("\n")
 
 clb3 = str(input("\ninsira o nome do colaborador 3: "))
 sex3 = str(input(f"\nInsira o sexo de {clb3}, H para Homem e M para Mulher : "))
 estcv3 = str(input(f"\ninsira estado civil de {clb3}:C para Casado e S para Solteiro : "))
-slr3 = float(input(f"\nInsira o salario atual de {clb3} : "))
+slr3 = float(input(f"\n\nInsira o salario atual de {clb3} : "))
+
+print("\n")
+
+
+#Processamento dos dados
 
 if sex1 == "H" and estcv1 == "S":
     newslr1 = slr1 * 1.20
     print(f"\no Salário de {clb1} passou de R${slr1} para R$ {newslr1}")
+elif sex1 == "M" and estcv1 == "C":
+    newslr1 = slr1 * 1.30
+    print(f"\no Salário de {clb1} passou de R${slr1} para R$ {newslr1}")
+else:
+    print(f"\nO(a) {clb1}, não faz jus ao aumento de salário")
+
+#colab 2
+
 if sex2 == "H" and estcv2 == "S":
     newslr2 = slr2 * 1.20
     print(f"\no Salário de {clb2} passou de R${slr2} para R$ {newslr2}")
+elif sex2 == "M" and estcv2 == "C":
+    newslr2 = slr2 * 1.30
+    print(f"\no Salário de {clb2} passou de R${slr2} para R$ {newslr2}")
+else:
+    print(f"\nO(a) {clb2}, não faz jus ao aumento de salário")
+
+#colab3
+       
 if sex3 == "H" and estcv3 == "S":
     newslr3 = slr3 * 1.20
     print(f"\no Salário de {clb3} passou de R${slr3} para R$ {newslr3}")
-
-if sex1 == "M" and estcv1 == "C":
-    newslr1 = slr1 * 1.30
-    print(f"\no Salário de {clb1} passou de R${slr1} para R$ {newslr1}")
-if sex2 == "M" and estcv2 == "C":
-    newslr2 = slr2 * 1.30
-    print(f"\no Salário de {clb2} passou de R${slr2} para R$ {newslr2}")
-if sex3 == "M" and estcv3 == "C":
+elif sex3 == "M" and estcv3 == "C":
     newslr3 = slr3 * 1.30
     print(f"\no Salário de {clb3} passou de R${slr3} para R$ {newslr3}")
+else:
+    print(f"\nO(a) {clb3}, não faz jus ao aumento de salário")
