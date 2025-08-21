7c45364f01918f5726908db76bc7a39823411c11
JORGE HENRIQUE INACIO DE OLIVEIRA <jorgehenrique18038@gmail.com>
2025-08-20 21:26:33 -0300

    corrigindo

--- valor-carros.py
+++ valor-carros.py
@@ -2,14 +2,13 @@ for i in range (2):
     model = input("\n\n\nInsira o modelo do carro a ser avaliado: ")
     fabrc = int(input(f"\nInsira o ano de fabricação do {model} :"))
     value = float(input(f"\ninsira o Valor de {model} : "))
-    if fabrc > 2020:
-        newvalue = value * 1.30
-    elif fabrc >= 2010 and fabrc < 2020:
-        newvalue = value * 1.20
-    elif fabrc < 2010:
+    if fabrc < 2010:
         newvalue = value * 1.10
+    elif fabrc <= 2020:
+        newvalue = value * 1.20
     else:
-        ()
+        newvalue = value * 1.30
+
     print(f"\n\no {model} passou de R${value} para {newvalue}")
 
 
