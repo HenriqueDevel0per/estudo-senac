edf144955170bc6f5306edce804163700779d39d
JORGE HENRIQUE INACIO DE OLIVEIRA <jorgehenrique18038@gmail.com>
2025-09-03 18:13:33 -0300

    Add Poo

--- conta-poo.py
+++ conta-poo.py
@@ -7,9 +7,8 @@ class Conta:
         self.cpf = input(f"Insira o seu CPF, {self.nome}: ")
         self.saldo = random.randint(0, 1000)  # saldo inicial aleatório
 
-    def depositar(self,deposito_inicial, valor):
-        deposito_inicial = float(input("Insira o valor a ser depositado: "))
-        self.depositar(deposito_inicial)  # usa o método para depositar
+    def depositar(self):
+        valor = float(input("Insira o valor a ser depositado: "))
         if valor > 0:
             self.saldo += valor
             print(f"Depósito de R${valor:.2f} realizado com sucesso!")
@@ -18,3 +17,10 @@ class Conta:
 
     def ver_saldo(self):
         print(f"{self.nome}, seu saldo atual é de R${self.saldo:.2f}")
+
+# Programa principal
+operar = Conta()      # já pede nome, conta e CPF
+operar.ver_saldo()    # mostra o saldo inicial
+
+operar.depositar()    # adiciona mais dinheiro
+operar.ver_saldo()    # mostra saldo atualizado
