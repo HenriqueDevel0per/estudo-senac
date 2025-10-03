5afc8733e9cabf99a7f8acdb3d0ae206a71759e1
Jorge Henrique Inacio de Oliveira <86747216+HenriqueDevel0per@users.noreply.github.com>
2025-10-03 18:48:12 -0300

    add materia backend

--- aula-1/ex06.py
+++ aula-1/ex06.py
@@ -1,14 +1,28 @@
 nome = input("insira seu nome: ") 
 sal_brt = float(input("Insira seu salário bruto : "))
-cpf = input("Insira seu CPF")
-inss = 0
-vale = 0
-sal_liq = 0
+cpf = input("Insira seu CPF: ")
 
+if sal_brt >= 1897.12:
+    inss = sal_brt * 0.11
+    cargo = "Acionista"
 
-if sal_brt >= 1897.50:
-    sal_brt = sal_brt * 0.10
-    sal_liq = sal_brt 
-    print(f"{sal_brt}")
+elif sal_brt < 1897.12 and sal_brt >= 1500:
+    inss = sal_brt * 0.09
+    cargo= "Gerente" 
 
- 
+elif sal_brt < 1500:
+    inss = sal_brt * 0.08
+    cargo = "Vendedor"
+
+else:
+    print("Opção invalida")
+
+if sal_brt >= 1500:
+    vale = sal_brt * 0.06
+
+elif sal_brt < 1500:
+    vale = sal_brt * 0.05
+
+sal_liq = sal_brt - (vale + inss)
+
+print(f"O {nome} de Cargo:{cargo} Salario Liquido: R$ {sal_liq} : Desconto Vale R$ {vale} Desconto INSS: R$ {inss}")
